9a5d18ba38df76e3682da78bda057ec5e64d8d51
raslenraslen <raslenm39@gmail.com>
2024-03-30 21:40:56 +0100

    thirdcommit

--- devreact/src/components/special.js
+++ devreact/src/components/special.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Specialitem from "./specialitem";
 
 const Special = () => {
   return (
     <div className=" bg-dark text-white   list-group">
       <u1>
+        <Specialitem
+          img="https://www.pinterest.com/pin/12736811440778344/"
+          width="200px"
+          height="700px"
+        />
+
         <li className="list-group-item">
-          <input type="checkbox" id="facebook" />
-          <label> Facebook </label>
-        </li>
-        <li className="list-group-item">
+          <img src="https://picsum.photos/30/20/?blur=2"></img>
           <input type="checkbox" id="twitter" />
           <label> twitter </label>
         </li>
---
+++ devreact/src/components/specialitem.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const specialitem = () => {
+  return (
+    <div>
+      <li className="list-group-item">
+        <img
+          src="https://www.pinterest.com/pin/12736811440778344/"
+          width="700px"
+        ></img>
+        <input type="checkbox" id="facebook" />
+
+        <label> Facebook </label>
+      </li>
+    </div>
+  );
+};
+
+export default specialitem;
