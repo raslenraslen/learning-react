4694f1a186537ec6d31e5e1ae06559323d612af4
raslenraslen <raslenm39@gmail.com>
2024-03-30 12:47:47 +0100

    secondcommit

---
+++ src/components/Text.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const style = {
+  color: "black ",
+  fontFamily: "auto ",
+};
+const Text = () => {
+  return (
+    <div style={style}>
+      <p> learning react </p>
+    </div>
+  );
+};
+
+export default Text;
