e6d04cfe32726acf158fabf605f5721a71e4eb08
Missaoui Raslen <149624976+raslenraslen@users.noreply.github.com>
2024-03-31 09:22:36 +0200

    Update team.js

--- reactl- level-2/membership/src/components/team.js
+++ reactl- level-2/membership/src/components/team.js
@@ -1,7 +1,67 @@
 import React from "react";
+import TeamMember from "./teamMember";
 
-const Team = () => {
-  return <div></div>;
-};
+function Team() {
+  return (
+    <div className="row">
+      <TeamMember
+        info={{
+          name: "Walter white",
+          img: "./images/walter.jpg",
+          phone: "+49 12345678",
+          website: "walterwhite.com",
+          position: "Bryan Cranston",
+        }}
+      />
+      <TeamMember
+        info={{
+          name: "Jesse Pinkman ",
+          img: "./images/jessie.jpg",
+          phone: "+49 78654264",
+          website: "JessePinkman.com",
+          position: "Aaron Paul",
+        }}
+      />
+      <TeamMember
+        info={{
+          name: "Hank white",
+          img: "./images/Hank.jpg",
+          phone: "+49 65646268",
+          website: "HankShrader.com",
+          position: "Dean Norris",
+        }}
+      />
+      <TeamMember
+        info={{
+          name: "Skyler White",
+          img: "./images/Skyler.jpg",
+          phone: "+49 4626866",
+          website: "Skylerwhite.com",
+          position: "Anna Gunn",
+        }}
+      />
+
+      <TeamMember
+        info={{
+          name: "Guss Fring",
+          img: "./images/guss.jpg",
+          phone: "+49 65646268",
+          website: "Giancarioesposito.com",
+          position: "Giancario esposito",
+        }}
+      />
+
+      <TeamMember
+        info={{
+          name: "Saul Goodman",
+          img: "./images/Saul.jpg",
+          phone: "+49 646268626",
+          website: "SaulGodman.com",
+          position: "Bob Odenkirk",
+        }}
+      />
+    </div>
+  );
+}
 
 export default Team;
