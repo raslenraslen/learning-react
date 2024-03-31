ba9f74baa514a38bd0911442f64dbe4db89fe852
Missaoui Raslen <149624976+raslenraslen@users.noreply.github.com>
2024-03-31 09:22:05 +0200

    Update header.js

--- reactl- level-2/membership/src/components/header.js
+++ reactl- level-2/membership/src/components/header.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="navbar bg-dark   rounded text-white">
+      <h2>Header</h2>
+    </div>
+  );
 };
 
 export default Header;
