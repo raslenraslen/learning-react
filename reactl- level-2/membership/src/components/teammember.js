2d29f4727e4adb2b357bb6aea2ca6435b3f44770
Missaoui Raslen <149624976+raslenraslen@users.noreply.github.com>
2024-03-31 09:20:44 +0200

    Update teammember.js

--- reactl- level-2/membership/src/components/teammember.js
+++ reactl- level-2/membership/src/components/teammember.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-const Teammember = () => {
-  return <div></div>;
+const Teammember = (props) => {
+  return (
+    <div className="col-md-4  col-sm-6  border">
+      <div className="card">
+        <div className="card-header">
+          <img
+            style={{
+              width: "300px",
+              height: "300px",
+              align: "center ",
+              marginLeft: "20px",
+            }}
+            src={props.info.img}
+          />
+          <div className="card-body">
+            <h1>{props.info.name} </h1>
+            <h3>{props.info.position} </h3>
+            <h4>{props.info.phone} </h4>
+            <h5>{props.info.website} </h5>
+            <h1>{props.info.email} </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Teammember;
